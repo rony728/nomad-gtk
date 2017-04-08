95081e4364403820c8d03de720092ce8ccf1a7cb
luis-lavaire <llavaire01@gmail.com>
2017-04-08 04:14:16 -0600

    Change name to 'nomad-gtk-theme'

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
-#! /bin/sh
+#! /bin/mksh
 
-for i in `cat "index"`; do
-	if [ ! -f "img/$i.png" ]; then
-		echo "Rendering icon $i.svg"
-		inkscape --export-png="img/$i.png" "img/svg/$i.svg" > /dev/null
-		inkscape --export-dpi=192 --export-png="img/$i@2.png" "img/svg/$i.svg" > /dev/null
+for i in `cat "src/index"`; do
+	if [ ! -f "gtk-3.0/img/$i.png" ]; then
+		inkscape --export-png="gtk-3.0/img/$i.png" "src/img/$i.svg" > /dev/null
+		inkscape --export-dpi=192 --export-png="gtk-3.0/img/$i@2.png" "src/img/$i.svg" > /dev/null
 	fi
 done
 
-for v in "gtk-3.0" "gtk-3.20"; do
-	for t in "" "-dark"; do
-		sassc --style compressed "${v}/scss/gtk${t}.scss" "${v}/gtk${t}.css"
-	done
+echo -e "\n --- DONE RENDERING IMAGES --- \n"
+
+for t in "" "-dark"; do
+	sassc "src/scss/gtk${t}.scss" "gtk-3.0/gtk${t}.css"
 done
+
+echo " --- DONE COMPILING SCSS --- \n"
