4bf9a47e8bea0a1bb4ef82c57ec7e0c5c666f8d3
luis-lavaire <llavaire01@gmail.com>
2017-05-14 22:20:56 -0600

    26236

--- build.sh
+++ build.sh
@@ -4,41 +4,6 @@
 # in the 'src' folder.
 # Whenever a file is modified; the theme is rebuilt.
 
-_render() {
-	for i in `cat "src/index"`; do
+killall gtk3-widget-factory &> /dev/null
 
-		img="gtk-3.0/img/$i"
-		src="src/img.svg"
-
-		if [ ! -f "$img.png" ]; then
-			inkscape -ji $i -e "$img.png" "$src" > /dev/null
-			inkscape -ji $i -d 192 -e "$img@2.png" "$src" > /dev/null
-		fi
-	done
-}
-
-_compile() {
-	sassc --style compact "src/scss/gtk.scss" "gtk-3.0/gtk.css"
-}
-
-_error() {
-	echo -e "\033[38;5;1m $@ \n"
-}
-
-_success() {
-	echo -e "\033[38;5;5m $@ \n"
-}
-
-_main() {
-	_render  ||
-	(_error ' - FAILED RENDERING IMAGES.'; exit) &&
-	_success ' - DONE RENDERING IMAGES.'
-
-	_compile ||
-	(_error ' - FAILED COMPILING SCSS.'; exit) &&
-	(_success ' - DONE COMPILING SCSS.'; gtk3-widget-factory)
-}
-
-_main
-
-find 'src/' | entr -r -c -d -p -s ${PWD}/build.sh
+find "src/" | entr -r -c -d -p -s "${PWD}/func.sh"
